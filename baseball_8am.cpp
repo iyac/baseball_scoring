4e2fb4501998bf425ac94c3e23cd4a7843ba8079
iyac <csgro@conestogac.on.ca>
2022-11-01 08:26:31 -0400

    Added getNum() definition

--- baseball_8am.cpp
+++ baseball_8am.cpp
@@ -5,3 +5,24 @@ int main(void)
 	printf("This program does nothing except display this message\n");
 	return 0;
 }
+
+#pragma warning(disable: 4996)
+int getNum(void)
+{
+	/* we will see in a later lecture how we can improve this code */
+	char record[121] = { 0 }; /* record stores the string */
+	int number = 0;
+	/* NOTE to student: indent and brace this function consistent with
+	your others */
+	/* use fgets() to get a string from the keyboard */
+	fgets(record, 121, stdin);
+	/* extract the number from the string; sscanf() returns a number
+	 * corresponding with the number of items it found in the string */
+	if (sscanf(record, "%d", &number) != 1)
+	{
+		/* if the user did not enter a number recognizable by
+		* the system, set number to -1 */
+		number = -1;
+	}
+	return number;
+}
